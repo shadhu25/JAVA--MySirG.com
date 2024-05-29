f2eaf7818a47d3aafa0da1a090e82d279068953d
shadhu25 <shadhushadhu25@gmail.com>
2024-05-29 11:36:48 +0530

    add CODES/4.wrapper repo

---
+++ CODES/4.wrapper/first.java
@@ -0,0 +1,12 @@
+public class first {
+    public static void main(String z[]){
+        Integer i1=Integer.valueOf("444");
+        Double d1=Double.valueOf("3.14");
+        int a=Integer.parseInt("345");
+        double b=Double.parseDouble("13.34");
+        System.out.println(i1.intValue());
+        System.out.println(d1.doubleValue());
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
