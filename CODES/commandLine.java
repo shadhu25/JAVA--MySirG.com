ac53b5f0eb1a160e53af3219ec488b4fc1833c7e
shadhu25 <shadhushadhu25@gmail.com>
2024-05-31 14:04:06 +0530

    add CODES/commandLine.java

---
+++ CODES/commandLine.java
@@ -0,0 +1,11 @@
+public class commandLine {
+    public static void main(String ram[]){
+        /* 
+        int m=0;
+        for(int i=0;i<ram.length;i++)
+             m=m+Integer.parseInt(ram[i]);
+        System.out.println("Sum is : "+m);*/
+        System.out.println(ram[0]);
+        System.out.println(ram[1]);
+    }
+}
